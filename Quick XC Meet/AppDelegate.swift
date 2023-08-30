67db7ed86b1dfcaff735649541a3cf85da45ae1d
bseaverclc <bseaver@d155.org>
2023-08-30 12:48:05 -0500

    Screen Stays on while using the app 2.3.1

--- Quick XC Meet/AppDelegate.swift
+++ Quick XC Meet/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        
+        application.isIdleTimerDisabled = true
         FirebaseApp.configure()
         Database.database().isPersistenceEnabled = true
         let meetsRef = Database.database().reference(withPath: "meets")
